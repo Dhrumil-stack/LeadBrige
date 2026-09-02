2bdb491a218ff383a06a4afb6747b41e935d80f3
DhrumilStack <sardisk12@gmail.com>
2026-09-02 19:53:27 +0530

    UserSerilizer

--- F_Activity_logs/serilizer.py
+++ F_Activity_logs/serilizer.py
@@ -3,6 +3,14 @@ from .models import ActivityLog
 
 
 class ActivitySerilizer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+
     class Meta:
             model=ActivityLog
             fields='__all__'
+
+    def get_user_name(self, obj):
+        if obj.user:
+            full_name = f"{obj.user.first_name} {obj.user.last_name}".strip()
+            return full_name if full_name else obj.user.email
+        return None
